ffc89a1e933f1b2c8728387b8657eba1aab440a5
YekaterinaAlex <ekaterina.alekseyenko@gmail.com>
2026-05-04 09:32:04 -0700

    refactor: convert Bug to functional component

--- src/components/Bug.tsx
+++ src/components/Bug.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
-
 type Props = {
   mustThrowError: boolean;
 };
 
-class Bug extends React.Component<Props> {
-  render() {
-    if (this.props.mustThrowError) {
-      throw new Error('Test Error');
-    }
-    return null;
+function Bug({ mustThrowError }: Props) {
+  return;
+  if (mustThrowError) {
+    throw new Error('Test Error');
   }
+  return null;
 }
 export default Bug;
