e71ef9fbb683f0ef2950456e42e5d96a6121e080
YekaterinaAlex <ekaterina.alekseyenko@gmail.com>
2026-06-20 08:44:13 -0700

    refactor: use localized navigation links

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import Link from 'next/link';
+import { Link } from '../../i18n/navigation';
 
 export const StyledHeader = styled.header`
   background-color: ${({ theme }) => theme.cardBackground};
