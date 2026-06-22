040ef28724189ea0a6f9638ae307e1ac5c5df529
YekaterinaAlex <ekaterina.alekseyenko@gmail.com>
2026-06-22 11:41:26 -0700

    fix: remove locale middleware routing

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Link } from '../../i18n/navigation';
+import Link from 'next/link';
 
 export const StyledHeader = styled.header`
   background-color: ${({ theme }) => theme.cardBackground};
